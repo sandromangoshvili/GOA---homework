be2c91f4106932a9fbed8237c2fb46c5cc28b3e7
sandromangoshvili <sandromangosvili@gmail.com>
2024-07-04 19:56:17 +0400

    second commit

---
+++ level 16/classswork/level 16.py
@@ -0,0 +1,52 @@
+num1 = 15.5
+num2 = 5.5
+
+print(num1 + num2)
+
+
+num3 = "15"
+num4 = "5"
+
+print(num3 + num4)
+
+current_age = int(input("current age:"))
+After_10_years = 10
+
+print(current_age + After_10_years)
+
+
+num = input("input int")
+num5 = input("input int")
+
+print(int(num) + int(num5))
+
+print(10 < 51 or 45 > 4)
+print(10 < 176 or 65 > 2)
+print(10 < 13 or 45 > 4)
+print(10 < 11 or 45 > 32)
+print(10 < 51 or 12 > 6)
+print(10 < 11 or 5 > 2)
+print(10 < 11 or 15 > 5)
+print(10 < 11 or 25 > 2)
+print(10 < 11 or 25 > 2)
+print(10 < 1231 or 55 > 45)
+print(10 < 11 or 13 > 45)
+
+
+
+
+
+
+print(10 < 2341 and 16 > 2)
+print(10 < 11 and 45 > 123)
+print(10 < 5431 and 15 > 76)
+print(10 < 2341 and 132 > 213)
+print(132 < 2341 and 15 > 435)
+print(10 < 11 and 13 > 2)
+print(10 < 123 and 35 > 234)
+print(10 < 31 and 12 > 2)
+print(10 < 18 and 16 > 123)
+print(10 < 99 and 13 > 53)
+
+
+
