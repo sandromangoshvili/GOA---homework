c8e2c2de9e2b0c83c03b8ecc075cbcfdb2059eec
sandromangoshvili <sandromangosvili@gmail.com>
2024-07-14 13:00:31 +0400

    second commit

---
+++ level 18/calsswork/level 18.py
@@ -0,0 +1,9 @@
+for i in range(30):
+    print("hellow world")
+
+for i in range(1,10):
+    print(i)
+
+for i in range(10,20,2):
+    print(i)
+
---
+++ level 18/homework/level 18.py
@@ -0,0 +1,52 @@
+# task 1
+
+# i = 0
+# for i in range(10): 
+#     i += 1
+#     print(i)
+#     print("hello world") 
+
+#task 2
+
+# for i in range(0,1000,100):
+#     print(i)
+
+
+#task 3
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+#task 4
+
+# i = 0
+# while i < 10: 
+#      i += 1
+#      print(i)
+#      print("hello world") 
+
+
+#task 5
+
+# number = 5
+# for i in range(10):
+#     number = number + i
+
+# print(number)
+
+#task 6
+
+# num = 0
+
+# while num <= 1000: 
+#     print(num) 
+#     num += 100 
+
+
+#task 7
+
+num = 100
+
+while num >= 0:  
+    print(num)
+    num -= 1
