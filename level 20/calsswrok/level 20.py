ff4b5a87abdddafa885969592cb892fbccc2033d
sandromangoshvili <sandromangosvili@gmail.com>
2024-07-21 13:45:04 +0400

    second commit

---
+++ level 20/calsswrok/level 20.py
@@ -0,0 +1,6 @@
+# age = 22 
+# if age >= 18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
---
+++ level 20/homewrok/level 20.py
@@ -0,0 +1,28 @@
+# num = int(input("Enter your number:"))
+
+# if num == 18:
+#     print("შენ მოიგე დინოზავრი")
+# elif num == 33:
+#     print("შენ მოიგე კურდღელი")
+# else:
+#     print("შენ მოიგე ჩემი ფეხები")
+
+
+# num = int(input("Enter your number"))
+
+# if num < 13:
+#     print("you are kid")
+# elif num > 13:
+#     print("You are teenager")
+# elif num > 20:
+#     print("You are grown up")
+
+
+
+
+
+for i in range(20):
+    if i <= 10:
+        print("hello world")
+    elif i >10:
+        print("goodbye world")
