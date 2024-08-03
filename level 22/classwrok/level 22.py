7b54d145e9ab2fe32e063f3c005d0197b1f12140
sandromangoshvili <sandromangosvili@gmail.com>
2024-08-03 20:39:46 +0400

    seocnd commit

---
+++ level 22/classwrok/level 22.py
@@ -0,0 +1,37 @@
+#        -5 -4 -3 -2 -1
+list1 = [1, 2, 3, 4, 5 ]
+#        0  1  2  3  4
+#          -3        -2       -1 
+list2 = ["python", "HTML", " Css"]
+#         0         1         2
+
+#          -3        -2       -1 
+list3 = ["sandro", "data", "irakli"]
+#          0          1          2      
+
+#        -3      -2     -1 
+list4 = [True, 14.4, False]
+#          0     1      2      
+
+
+print(list1[-5])
+print(list1[0])
+
+
+print(list2[-3])
+print(list2[1])
+
+
+print(list3[-3])
+print(list3[1])
+
+
+print(list4[-3])
+print(list4[1])
+
+
+
+
+list3[2] = "erekle"
+print(list3)
+
---
+++ level 22/homework/level 22.py
@@ -0,0 +1,63 @@
+#          -0
+list1 = [1, "???"]
+
+#          -0 -1
+list2 = [1, 2]
+
+#          -0 -1 -2
+list3 = [1, 2, 3]
+
+#          -0 -1 -2 -3
+list4 = [1, 2, 3, 4]
+
+#          -0 -1 -2 -3 -4
+list5 = [1, 2, 3, 4, 5]
+
+#          -0 -1 -2 -3 -4 -5
+list6 = [1, 2, 3, 4, 5, 6]
+
+#          -0 -1 -2 -3 -4 -5 -6
+list7 = [1, 2, 3, 4, 5, 6, 7]
+
+#          -0 -1 -2 -3 -4 -5 -6 -7
+list8 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+#          -0 -1 -2 -3 -4 -5 -6 -7 -8
+list9 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#          -0 -1 -2 -3 -4 -5 -6 -7 -8 -9
+list10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+print(list1[1])
+print(list1[1])
+
+
+print(list2[-2])
+print(list2[1])
+
+
+print(list3[-2])
+print(list3[1])
+
+
+print(list4[-2])
+print(list4[1])
+
+
+
+
+list10[2] = "sandro"
+print(list3)
+
+
+list7[2] = "sandro"
+print(list3)
+
+list1[1] = "sandro"
+print(list3)
+
+
+list5[2] = "sandro"
+print(list3)
