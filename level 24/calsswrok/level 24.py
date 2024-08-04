708e710af09f12609f9fd32480a6f6675b67d300
sandromangoshvili <sandromangosvili@gmail.com>
2024-08-04 12:48:28 +0400

    second commit

---
+++ level 24/calsswrok/level 24.py
@@ -0,0 +1,106 @@
+listn = [True , "helo" , 15 , 10.1 ]
+
+print(listn[0:2])
+print(listn[1:3])
+print(listn[2:])
+
+
+#task 2
+
+
+list1 = [True , False , "hello world" , 15 , 5.1 , 4 , 8 , "bye world" , "novatori<GOA"]
+#          0      1          2           3     4   5    6     7               8
+
+print(list1[1:5])
+print(list1[3:7])
+print(list1[4:8])
+
+
+#task 3
+
+# ჭრის რავი ტასკ 2შია  მაგალითი
+
+
+# task 4
+
+num1 = 5
+num2 = 7
+
+print(num1 + num2)
+
+
+# task 5
+
+str1 = "sandro_"
+str2 = "mango"
+
+print(str1 + str2)
+
+# თუ გინდა რო გაიგო ჩანაწერს უყურე
+
+#task 6
+
+num3 = 10
+num4 = 2 
+
+print(num3 / num4)
+
+# ჩანაწერებს რატო არ უყურებ !!!
+
+
+# task 7
+
+# brain error!!! ＼（〇_ｏ）／
+
+
+# task 8
+
+print(5 + 5 == 8  + 2)
+
+
+# task 9 
+
+# =< ; => ; == ; ** ; მეტი ზეპირად აღარ მახსოვს ავნეზია მაქ o((⊙﹏⊙))o.
+
+
+# task 10
+
+print(10 < 51 or 45 > 4)
+print(10 < 176 or 65 > 2)
+print(10 < 13 or 45 > 4)
+print(10 < 11 or 45 > 32)
+print(10 < 51 or 12 > 6)
+
+
+
+# task 11 
+
+for i in range(11):
+    print(i)
+
+
+# task 12
+
+list5 = [1, 2, 3, 4, 5, 6, 7, 8 , 9]
+
+for i in list5:
+    print(i)
+
+
+# task 13
+
+list6 = ["sandro", "zuka" , "data" , "nika" , "luka" , "atuka" ,"erekle"]
+
+
+for i in list6:
+    print
+
+#task 14 
+
+
+
+my_list = ["sandro", "esne unda mekna", "sworia?"]
+
+list7 = my_list[::-1]
+
+print(list7)
