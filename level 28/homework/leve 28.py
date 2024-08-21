2143c7a1b6e0f6baa963813325ebbf8bcbd92675
sandromangoshvili <sandromangosvili@gmail.com>
2024-08-21 01:24:20 -0700

    second commit

---
+++ level 28/homework/leve 28.py
@@ -0,0 +1,9 @@
+# .upper()
+# .lower()
+# .capitalize()
+# .swapcase()
+# .find()
+# len() - თვლის რამდენი ელემენტია
+# .append() - list'ის ბოლოში ამატებს ელემენტს
+# .insert() - list'ში ამატებს
+# .pop() - შლის list'იდან ელემენტს
