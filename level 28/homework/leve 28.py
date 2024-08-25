545f1677c4e8e3b3f55db905da1be0c3e15e6144
sandromangoshvili <sandromangosvili@gmail.com>
2024-08-25 06:34:23 -0700

    second commit

--- level 28/homework/leve 28.py
+++ level 28/homework/leve 28.py
@@ -1,3 +1,4 @@
+# 2) გატესტეთ ყველა დღეს ნასწავლი ფუნქცია
 # .upper()
 # .lower()
 # .capitalize()
@@ -7,3 +8,67 @@
 # .append() - list'ის ბოლოში ამატებს ელემენტს
 # .insert() - list'ში ამატებს
 # .pop() - შლის list'იდან ელემენტს
+# print("football".upper())
+# print("FOOTBALL".lower())
+# print("fooTBAll".capitalize())
+# print("FoOtBAlL".swapcase())
+
+# football = "football"
+# print(football.find("b"))
+
+# list1 = ["doom", "fifa12", "callofduty"]
+# print(len(list1))
+
+# list1.append("pes14")
+# print(list1)
+
+# list1.insert(2, "gta5")
+# print(list1)
+
+# list1.pop(2)
+# print(list1)
+# [4:17 AM]
+#3) კომენტარებით ახსენით რას აკეთებს თითოეული ფუნქცია (ინგლისურად უკეთესი იქნება)
+
+#. upper()
+#upper makes all of the letters in the string capital, like: football --> FOOTBALL
+
+ #.lower()
+#lower makes all of the letters in the string small, like:  FOOTBALL -->  football
+
+ # .capitalize()
+#capitalize makes letter in the string capital, like: football --> Football
+
+ # .swapcase()
+#swapcase swaps small letters with bigs and contrary, like:  FoOtBAlL -->  fOoTbalL
+
+ # .find()
+#find finds the letter in the string using indexing
+
+len()
+#count the elements in the list or symbols in the string
+
+# .append()
+#append adds the element to the end of the list
+
+# .insert()
+#insert adds the element to the list to the place you want
+
+# .pop()
+#pop deletes the elements from the list
+# [4:18 AM]
+#4) შექმენით რაიმე list'ი და გამოიყენეთ მასზე ყველა list'ის ფუნქცია რაც ვისწავლეთ, თან ნაბიჯ-ნაბიჯ ყველა ხაზი ახსენით
+
+list1 = ["doom", "fifa12", "callofduty"]
+print(len(list1))
+
+list1[0] = "minecraft"
+
+list1.append("pes14")
+print(list1)
+
+list1.insert(2, "gta5")
+print(list1)
+
+list1.pop(2)
+print(list1)
