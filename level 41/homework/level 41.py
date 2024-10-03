4692ea870da5e4d66f4341d1354589e6767dfc93
sandromangoshvili <sandromangosvili@gmail.com>
2024-10-03 20:11:25 +0400

    second commit

---
+++ level 41/homework/level 41.py
@@ -0,0 +1,10 @@
+# list1 = [5, 10, 15, 20, 25]
+# list1.append("supergela")
+# list1.pop()
+# print(list1)
+
+
+tuple1 = (1, 2, 3, 4, 5, 6)
+tuple1.__class_getitem__
+print(tuple1)
+# ბიზი კონტორიოლო აი ეს წაიღე და მე თავი გამანებე ❤
