244abd25a9592f5564fe5bf7f40c2d361fb0a492
sandromangoshvili <sandromangosvili@gmail.com>
2024-10-17 19:53:52 +0400

    second commit

---
+++ level 45/homewrok/level 45.py
@@ -0,0 +1,54 @@
+my_dict = {
+    "name": "Sandro",
+    "age": 12,
+    "city": "Tbilisi",
+    "school": "182",
+    "hobby": "programming",
+    "favorite_color": "red",
+    "pet": "dead",
+    "food": "pizza",
+    "sport": "football",
+    "music": "rock"
+}
+
+
+for key, value in my_dict.items():
+    print(key)
+
+
+
+
+    my_dict1 = {
+    "name": "Sandro",
+    "age": 12,
+    "city": "Tbilisi",
+    "school": "182",
+    "hobby": "programming",
+    "favorite_color": "red",
+    "pet": "dead",
+    "food": "pizza",
+    "sport": "football",
+    "music": "rock"
+}
+
+
+for key, value in my_dict1.items():
+    print(value)
+
+
+
+
+   
+result = [(x // 2) + 1 for x in range(50, 101)]
+
+
+print(result)
+
+
+
+
+
+result = [(x * 2) + 1 for x in range(50, 101)]
+
+
+print(result)
